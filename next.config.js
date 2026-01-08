bb08f671eb4c0e85e07aae7d81169e7265c9e569
Teodor Datcu <teodordatcu.dtc@gmail.com>
2026-01-08 11:35:58 +0200

    Add Supabase storage domain to Next.js image config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
   },
   // Supabase is used as the database, no webpack externals needed
 }
